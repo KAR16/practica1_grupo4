59de60a09777b6155cb033c081728925f9d161f8
WilmerJD <wjuand@miumg.edu.gt>
2023-10-14 01:03:50 -0600

    Actualizacion raiz cubica 7690207733

--- potencia-raiz-cubica.js
+++ potencia-raiz-cubica.js
@@ -4,6 +4,18 @@ function potenciaCubica(num) {
     return numero;
 }
 
-let num = 4;
+function raizCubica(num){
+    console.log("Programa sacar la raíz cúbica de un número");
+    var numero = Math.round(Math.pow(num, 1/3));
+    return numero;
+}
+
+let num = 125;
+
+//Función Potencia Cúbica
 let numElevadoCubo = potenciaCubica(num);
 console.log(num + " elevado al cubo = "+ numElevadoCubo);
+
+//Función Raíz Cúbica
+let numRaizCubica = raizCubica(num);
+console.log("La raíz cúbica de "+ num +" es = "+ numRaizCubica);
