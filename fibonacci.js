a4bc8de1e1266dea4b43927fc3c78823c4b64af4
KAR16 <kherrera16@gmail.com>
2023-10-13 23:59:49 -0600

    Función de números primos añadida al archivo fibonacci-7690-20-8093

--- fibonacci.js
+++ fibonacci.js
@@ -1,16 +1,40 @@
-console.log("Programa para determinar un Fibonacci");
-
-function fibonacci(limit){
+function fibonacci(limit) {
+    console.log("Programa para determinar un Fibonacci");
     const arrFib = [0, 1];
     let fib;
-    for (let i = 2; i<= limit; i++){
+    for (let i = 2; i <= limit; i++) {
         console.log(i);
         fib = arrFib[i - 1] + arrFib[i - 2];
         arrFib.push(fib);
     }
-    console.log("El fibonacci de "+ limit + " es: "+ fib);
+    console.log("El fibonacci de " + limit + " es: " + fib);
     return arrFib;
 }
 
-const testFib = fibonacci(3);
+function esPrimo(numero) {
+    console.log("Programa para determinar si un número es Primo");
+    if (numero <= 1) {
+        return false;
+    }
+    // Comprobamos si el número es divisible por algún número menor que él mismo
+    for (let i = 2; i < numero; i++) {
+        if (numero % i === 0) {
+            return false;
+        }
+    }
+    return true;// Si no ha sido divisible por ningún número, es primo return true;
+}
+
+let num = 4;
+
+//Función Fibonacci
+const testFib = fibonacci(num);
 console.log(testFib);
+
+//Función Primo
+if (esPrimo(num)) {
+    console.log(num + " es primo");
+} else {
+    console.log(num + " no es primo");
+}
+
