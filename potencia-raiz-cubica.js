433fb68044c8feee4c0839ac043a8c5a037cc626
KAR16 <kherrera16@gmail.com>
2023-10-14 02:41:32 -0600

    Se añadió la función de raiz y potencia al cuadrado 7690-20-8093

--- potencia-raiz-cubica.js
+++ potencia-raiz-cubica.js
@@ -4,9 +4,21 @@ function potenciaCubica(num) {
     return numero;
 }
 
+function potenciaCuadrada(num){
+    console.log("Programa elevar al cuadrado un número");
+    var numero = Math.pow(num, 2);
+    return numero;
+}
+
 function raizCubica(num){
     console.log("Programa sacar la raíz cúbica de un número");
     var numero = Math.round(Math.pow(num, 1/3));
+    return numero;
+}
+
+function raizCuadrada(num){
+    console.log("Programa sacar la raíz cuadrada de un número");
+    var numero = Math.round(Math.pow(num, 1/2));
     return numero;
 }
 
@@ -14,8 +26,16 @@ let num = 125;
 
 //Función Potencia Cúbica
 let numElevadoCubo = potenciaCubica(num);
-console.log(num + " elevado al cubo = "+ numElevadoCubo);
+console.log(num + " elevado al cubo = "+ numElevadoCubo + "\n");
+
+//Función Potencia Cuadrada
+let numElevadoCuadrado = potenciaCuadrada(num);
+console.log(num + " elevado al cuadrado = "+ numElevadoCuadrado+ "\n");
 
 //Función Raíz Cúbica
 let numRaizCubica = raizCubica(num);
-console.log("La raíz cúbica de "+ num +" es = "+ numRaizCubica);
+console.log("La raíz cúbica de "+ num +" es = "+ numRaizCubica+ "\n");
+
+//Función Raíz Cúbica
+let numRaizCuadrada = raizCuadrada(num);
+console.log("La raíz cuadrada de "+ num +" es = "+ numRaizCuadrada+ "\n");
