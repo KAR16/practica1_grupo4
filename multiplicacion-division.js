a89c74e73b0f79d913570e5ec566316e33058145
unknown <msuchiteg@miumg.edu.gt>
2023-10-14 12:57:38 -0600

    se añadio la función para calcular el módulo (resto) de dos números bf/2490-19-18309

--- multiplicacion-division.js
+++ multiplicacion-division.js
@@ -17,7 +17,7 @@ function division(num1, num2){
 }
 
 let num1 = 25;
-let num2 = 0
+let num2 = 3
 
 //Función para multiplicar
 let resMultiplicacion = multiplicacion(num1, num2);
@@ -26,3 +26,37 @@ console.log(num1 + " * "+ num2 + " = "+ resMultiplicacion);
 //Función para dividir
 let resDivision = division(num1, num2);
 console.log(num1 + " / "+ num2 +" = "+ resDivision);
+
+// Función para calcular el módulo de dos números
+function modulo(num1, num2) {
+    console.log("Programa para calcular el módulo de 2 números");
+    result = 0;
+    if (num2 !== 0) {
+        result = num1 % num2;
+    } else {
+        console.log("Error: No se puede calcular el módulo por cero.");
+        result = null;
+    }
+    return result;
+}
+
+// Función para calcular el módulo
+let resModulo = modulo(num1, num2);
+console.log(num1 + " % " + num2 + " = " + resModulo);
+
+/ Función para calcular el módulo de dos números
+function modulo(num1, num2) {
+    console.log("Programa para calcular el módulo de 2 números");
+    result = 0;
+    if (num2 !== 0) {
+        result = num1 % num2;
+    } else {
+        console.log("Error: No se puede calcular el módulo por cero.");
+        result = null;
+    }
+    return result;
+}
+
+// Función para calcular el módulo
+let resModulo = modulo(num1, num2);
+console.log(num1 + " % " + num2 + " = " + resModulo);
