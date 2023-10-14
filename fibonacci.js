f5358560b590512538ecf21dbc9679f237558870
angelrm22 <aromerom4@miumg.edu.gt>
2023-10-14 12:16:42 -0600

    Se añadio la funcion para determinar si un numero es primo en un rango determinado en fibonacci 7690-20-16330

--- fibonacci.js
+++ fibonacci.js
@@ -38,3 +38,42 @@ if (esPrimo(num)) {
     console.log(num + " no es primo");
 }
 
+// Función para determinar si un número es primo
+function esPrimo(numero) {
+    console.log("Programa para determinar si un número es Primo");
+    if (numero <= 1) {
+        return false;
+    }
+    // Comprobamos si el número es divisible por algún número menor que él mismo
+    for (let i = 2; i < numero; i++) {
+        if (numero % i === 0) {
+            return false;
+        }
+    }
+    return true; // Si no ha sido divisible por ningún número, es primo
+}
+
+// Función para generar una lista de números primos en un rango dado
+function generarPrimosEnRango(inicio, fin) {
+    console.log("Programa para generar números primos en un rango");
+    const numerosPrimos = [];
+
+    for (let numero = inicio; numero <= fin; numero++) {
+        if (esPrimo(numero)) {
+            numerosPrimos.push(numero);
+        }
+    }
+
+    return numerosPrimos;
+}
+
+// Definir el rango en el que deseas buscar números primos
+const rangoInicio = 10;
+const rangoFin = 50;
+
+// Llamar a la función para generar números primos en el rango especificado
+const primosEnRango = generarPrimosEnRango(rangoInicio, rangoFin);
+
+// Imprimir los números primos encontrados en el rango
+console.log("Números primos en el rango de " + rangoInicio + " a " + rangoFin + ":");
+console.log(primosEnRango);
