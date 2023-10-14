2d85d7904a0b4ae72c705065db84e8df1a67deea
Yesenia <mgarciap31@miumg.edu.gt>
2023-10-14 02:41:33 -0600

    Se ha añadido la Funcion de Suma y Resta 7690-2012268

---
+++ Función_suma_resta.js
@@ -0,0 +1,14 @@
+function suma(a, b) {
+  return a + b;
+}
+
+function resta(a, b) {
+  return a - b;
+}
+
+// Ejemplo de uso de las funciones de suma y resta
+const num1 = 10;
+const num2 = 5;
+
+console.log(`La suma de ${num1} y ${num2} es: ${suma(num1, num2)}`);
+console.log(`La resta de ${num1} y ${num2} es: ${resta(num1, num2)}`);
